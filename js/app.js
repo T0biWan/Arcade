e6598078b2c6424fe1e4bc82ac7b88774791f4d5
Tobi Wan <Tobi.Klatt@web.de>
2017-01-28 20:43:50 +0100

    Made simple player-sprite

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@
  * Created by Tobi on 28.01.2017.
  */
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', {preload: preload, create: create, update: update });
+var player;
 
-function preload() {}
-function create() {}
+function preload() {
+    game.load.image('player', 'media/images/player.png');
+}
+
+function create() {
+    player = game.add.sprite(0, 0, 'player');
+    player.height = 80;
+    player.width = 40;
+}
+
 function update() {}
