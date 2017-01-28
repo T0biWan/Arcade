ab315d8d5aa42e9f9097f2d6f2b85886f5af3dae
Tobi Wan <Tobi.Klatt@web.de>
2017-01-28 21:06:04 +0100

    Movement for player implemented

--- js/app.js
+++ js/app.js
@@ -3,15 +3,31 @@
  */
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', {preload: preload, create: create, update: update });
 var player;
+var wKey;
 
 function preload() {
     game.load.image('player', 'media/images/player.png');
 }
 
 function create() {
-    player = game.add.sprite(0, 0, 'player');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    player = game.add.sprite(game.width/2, game.height/4, 'player');
     player.height = 80;
     player.width = 40;
+    player.anchor.set(0.5, 0.5);
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
+    //wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    //cursors.up = game.input.keyboard.addKey(Phaser.Keyboard.W);
 }
 
-function update() {}
+function update() {
+    player.body.velocity.x = 0;
+    if (cursors.left.isDown) player.body.velocity.x = -150;
+    else if (cursors.right.isDown) player.body.velocity.x = 150;
+    if (cursors.up.isDown) player.body.velocity.y = -150;
+}
